19fc72c006061f787ea97e385c0a55c90d15b94f
adityaexe <kmradi2002@gmail.com>
2025-06-20 12:51:40 +0530

    Refactor routing in App component and wrap with BrowserRouter

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import LoginPage from "./Pages/LoginPage";
+import ProfilePage from "./Pages/ProfilePage";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/login"
+          element={
+            <div>
+              <LoginPage />
+            </div>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <div>
+              <ProfilePage />
+            </div>
+          }
+        />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
