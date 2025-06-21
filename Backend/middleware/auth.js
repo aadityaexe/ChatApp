5b7ad312fbf90ece4d0bd155fe220993c7953029
adityaexe <kmradi2002@gmail.com>
2025-06-22 02:52:15 +0530

    Add authentication middleware to protect routes with JWT verification

---
+++ Backend/middleware/auth.js
@@ -0,0 +1,28 @@
+// Middleware to protect
+
+import jwt from "jsonwebtoken";
+import User from "../models/User";
+import e from "express";
+
+export const protectRoute = async (req, res, next) => {
+  try {
+    const token = req.headers.token;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.userId).select("-password");
+
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "User not found",
+      });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log(error.message);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
