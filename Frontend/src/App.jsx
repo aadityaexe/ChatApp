5acfeaf126284aab07503e3fb0d3c350288320a9
adityaexe <kmradi2002@gmail.com>
2025-06-22 20:29:03 +0530

    Refactor App component to manage user authentication state and navigation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,29 +1,30 @@
 import { Toaster } from "react-hot-toast";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import LoginPage from "./Pages/LoginPage";
 import ProfilePage from "./Pages/ProfilePage";
+import { useContext } from "react";
+import { AuthContext } from "./Context/AuthContext";
 
 const App = () => {
+  const { authUser } = useContext(AuthContext);
+
   return (
     <div className="bg-[url('/src/assets/bgImage.svg')] bg-contain">
       <Toaster />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
         <Route
           path="/login"
-          element={
-            <div>
-              <LoginPage />
-            </div>
-          }
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
         />
         <Route
           path="/profile"
           element={
-            <div>
-              <ProfilePage />
-            </div>
+            <div>{!authUser ? <ProfilePage /> : <Navigate to="/login" />}</div>
           }
         />
       </Routes>
