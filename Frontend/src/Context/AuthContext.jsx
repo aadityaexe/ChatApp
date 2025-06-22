ccdba6ef6296882b4cd7a71ade0729fc46b1dc0b
adityaexe <kmradi2002@gmail.com>
2025-06-22 13:11:16 +0530

    Refactor AuthContext to include socket connection and user authentication logic

--- Frontend/src/Context/AuthContext.jsx
+++ Frontend/src/Context/AuthContext.jsx
@@ -1,5 +1,8 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import axios from "axios";
+
+import io, { connect } from "socket.io-client";
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 axios.defaults.baseURL = backendUrl;
@@ -7,15 +10,75 @@ export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem("token"));
-
   const [authUser, setAuthUser] = useState(null);
-  const [onlineUser, setOnlineUser] = useState([]);
+  const [onlineUsers, setOnlineUsers] = useState([]);
   const [socket, setSocket] = useState(null);
+
+  // Check if user is authenticated and if so, see the user data and connect the socket
+
+  const checkAuth = async () => {
+    try {
+      const { data } = await axios.get("/api/auth/check");
+
+      if (data.success) {
+        setAuthUser(data.user);
+        connectSocket(data.user);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  // Login function to handle user authentication and socket connection
+
+  const login = async (state, credentials) => {
+    try {
+      const { data } = await axios.post(`/api/auth/${state}`, credentials);
+
+      if (data.success) {
+        setAuthUser(data.userData);
+        connectSocket(data.userData);
+        axios.defaults.headers.common["token"] = data.token;
+        setToken(data.token);
+        localStorage.setItem("token", data.token);
+        toast.success(data.message);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  // Connect socket function to handle socket connection and online user update
+
+  const connectSocket = (userData) => {
+    if (!userData || socket?.connected) return;
+    const newSocket = io(backendUrl, {
+      query: {
+        userId: userData._id,
+      },
+    });
+
+    newSocket.connect();
+    setSocket(newSocket);
+
+    newSocket.on("getOnlineUsers", (userIds) => {
+      setOnlineUsers(userIds);
+    });
+  };
+
+  useEffect(() => {
+    if (token) {
+      axios.defaults.headers.common["token"] = token;
+    }
+    checkAuth();
+  }, []);
 
   const value = {
     axios,
     authUser,
-    onlineUser,
+    onlineUsers,
     socket,
   };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
