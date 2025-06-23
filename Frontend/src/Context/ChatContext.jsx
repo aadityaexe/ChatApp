666508c4c81bbb0acfc639de2d8fb9f57ca7f064
adityaexe <kmradi2002@gmail.com>
2025-06-23 15:02:53 +0530

    Add getMessages function to fetch messages for selected user

--- Frontend/src/Context/ChatContext.jsx
+++ Frontend/src/Context/ChatContext.jsx
@@ -29,6 +29,20 @@ export const ChatProvider = ({ children }) => {
     }
   };
 
+  // function to get messages for selected user
+
+  const getMessages = async (userId) => {
+    try {
+      const { data } = await axios.get(`/api/messages/${userId}`);
+
+      if (data.success) {
+        setMessages(data.messages);
+      }
+    } catch (error) {
+      toast.error(error.messages);
+    }
+  };
+
   const value = {};
   return <ChatContext.Provider value={value}>{children}</ChatContext.Provider>;
 };
