e80fc8cd66846d6912114fcb2c7eb58c0ca7ba00
adityaexe <kmradi2002@gmail.com>
2025-06-26 15:59:00 +0530

    Remove unused useState import from HomePage component

--- Frontend/src/Pages/HomePage.jsx
+++ Frontend/src/Pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import Sidebar from "../Component/Sidebar";
 import ChatContainer from "../Component/ChatContainer";
 import RightSidebar from "../Component/RightSidebar";
