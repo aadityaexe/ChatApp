362b14f471c60878c23d67051e6b1d5dac0bbb21
adityaexe <kmradi2002@gmail.com>
2025-06-26 15:59:11 +0530

    Update ProfilePage styles for improved background color

--- Frontend/src/Pages/ProfilePage.jsx
+++ Frontend/src/Pages/ProfilePage.jsx
@@ -29,7 +29,7 @@ const ProfilePage = () => {
 
   return (
     <div className="min-h-screen bg-cover bg-no-repeat flex items-center justify-center">
-      <div className="w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg">
+      <div className="w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg bg-gray-900">
         <form
           onSubmit={onSubmitHandler}
           action=""
